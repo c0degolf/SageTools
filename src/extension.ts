49840186e9b5d4d8ac1ae39d15dadbf74af1d953
c0degolf <pyssw0423@naver.com>
2024-07-29 18:27:09 +0900

    comment

--- src/extension.ts
+++ src/extension.ts
@@ -91,52 +91,6 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(SageCompile);
-
-    let SageComment = vscode.commands.registerCommand('SageTools.comment', () => {
-        const editor = vscode.window.activeTextEditor;
-    
-        if (editor) {
-            const document = editor.document;
-            const selections = editor.selections;
-    
-            editor.edit(editBuilder => {
-            let shouldAddComment = false;
-    
-            // 선택된 모든 줄을 검사하여 하나라도 주석이 없는 줄이 있는지 확인
-            selections.forEach(selection => {
-                for (let i = selection.start.line; i <= selection.end.line; i++) {
-                    const line = document.lineAt(i);
-                    const text = line.text;
-                    if (!text.trim().startsWith('#')) {
-                        shouldAddComment = true;
-                        break;
-                    }
-                }
-            });
-    
-            // 모든 선택된 줄에 대해 주석을 추가 또는 제거
-            selections.forEach(selection => {
-                for (let i = selection.start.line; i <= selection.end.line; i++) {
-                    const line = document.lineAt(i);
-                    const text = line.text;
-    
-                    if (shouldAddComment) {
-                        // 주석이 없는 줄이 하나라도 있다면 모든 줄에 주석 추가
-                        editBuilder.insert(line.range.start, '# ');
-                    } else {
-                        // 모든 줄이 주석이라면 주석 제거
-                        const commentIndex = text.indexOf('#');
-                        editBuilder.delete(new vscode.Range(
-                            new vscode.Position(i, commentIndex),
-                            new vscode.Position(i, commentIndex + 2)
-                        ));
-                    }
-                }});
-            });
-        }
-    });
-    
-    context.subscriptions.push(SageComment);
 }
 
 export function deactivate() {}
